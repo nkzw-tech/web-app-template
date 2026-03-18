798b7611e1c78c7bf03670c653e952808c3f4964
cpojer <christoph.pojer@gmail.com>
2026-03-18 10:41:02 +0900

    Update deps.

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,17 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import reactCompiler from 'babel-plugin-react-compiler';
 import { defineConfig } from 'vite';
+import babel from 'vite-plugin-babel';
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
-    react({
-      babel: {
+    babel({
+      babelConfig: {
         plugins: [reactCompiler],
         presets: [fbteePreset],
       },
     }),
+    tailwindcss(),
+    react(),
   ],
 });
